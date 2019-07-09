505cf97419c2255c25be95bc016e284105a8a739
Stryde <stryde@Strydes-MacBook-Pro-2.local>
2019-07-08 22:01:43 -0600

    add front-end

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,26 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.Port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
-
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-});
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on the  port:  ${port}`)
+    console.log(`Server is running on port: ${port}`);
 });
 
